1f22c99b609edf6ae481905c0f4ba67c8a0a551e
Minsoek96 <FE.minseok96@gmail.com>
2023-08-31 04:29:16 +0900

    fix : 이슈 페이지 컨텍스트 및 리듀서 정상 구동 확인 & 페이지내 오류, 로딩 구현

--- src/components/issues/IssuesList.js
+++ src/components/issues/IssuesList.js
@@ -1,25 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { getIssues } from "../../apis/issues";
 import IssuesItem from "./IssuesItem";
 import Advertisement from "../Advertisement";
+import {
+  IssueDispatchContext,
+  IssueStateContext,
+} from "../../context/IssuesContext";
+import { useNavigate } from "react-router-dom";
 
 const IssuesList = () => {
-  const [data, setData] = useState();
+  const { issueList, isError, isLoading } = useContext(IssueStateContext);
+  const dispatch = useContext(IssueDispatchContext);
+
   useEffect(() => {
-    getIssues().then((res) => setData(res.data));
+    dispatch && getIssues(dispatch);
   }, []);
+
+  const renderIssue = (issue, index) => (
+    <React.Fragment key={issue.id}>
+      {(index + 1) % 5 === 0 && <Advertisement />}
+      <IssuesItem {...issue} />
+    </React.Fragment>
+  );
+
+  if (isError) {
+    return <p>에러확인</p>;
+  }
+
+  if (isLoading) {
+    return <p>로딩중 ...</p>;
+  }
+
   return (
     <div>
       <h2>이슈 페이지</h2>
-      <div>
-        {data &&
-          data.map((issue, index) => (
-            <React.Fragment key={issue.id}>
-              {(index + 1) % 5 === 0 && <Advertisement />}
-              <IssuesItem {...issue} />
-            </React.Fragment>
-          ))}
-      </div>
+      <div>{issueList.map(renderIssue)}</div>
     </div>
   );
 };
