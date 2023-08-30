e7bad24ae4ba5d1a6b32dfc891c713d5dc1fe15f
Minsoek96 <FE.minseok96@gmail.com>
2023-08-31 06:18:21 +0900

    fix : api 세부 요구사항 조건 구현 (open 상태, 코멘트 많은 순)에 맞게 데이터 요청 및 표시) *추후에 드롭박스로 옵션 설정을 고려하여 default값 설정*

--- src/apis/issues.js
+++ src/apis/issues.js
@@ -2,12 +2,19 @@ import { IssueAction } from "../reducer/IssuesReducer";
 import { API } from "./client";
 import { ISSUES } from "./config";
 
-export const getIssues = async (dispatch) => {
+export const getIssues = async (
+  dispatch,
+  page,
+  status = "open",
+  sort = "comments"
+) => {
   try {
     dispatch({
       type: IssueAction.ISSUE_LIST_REQUEST,
-    })
-    const response = await API(ISSUES);
+    });
+    const response = await API(
+      `${ISSUES}?status=${status}&sort=${sort}&page=${page}&per_page=15`
+    );
     if (response) {
       dispatch({
         type: IssueAction.ISSUE_LIST_SUCCESS,
