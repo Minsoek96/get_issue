0a0c81bad475c576683739caea86d8a6f17aaae4
Minsoek96 <FE.minseok96@gmail.com>
2023-08-31 10:37:43 +0900

    feat : 로딩 지연을 나타내기 위한 Spinner 구현

---
+++ src/components/Spinner.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+// 스피너 애니메이션
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+const SpinnerStyle = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SpinnerWrapper = styled.div`
+  display: inline-block;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  border: 3px solid rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+  border-top: 3px solid #000;
+  animation: ${spin} 1s linear infinite;
+`;
+
+const Spinner = () => {
+  return (
+    <SpinnerStyle>
+      <SpinnerWrapper />
+      <h2>Loading...</h2>
+    </SpinnerStyle>
+  );
+};
+
+export default Spinner;
