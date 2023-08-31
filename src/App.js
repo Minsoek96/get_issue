5d05e28f0aad0d3a295328ef4c3ffdab8f73aef9
Minsoek96 <FE.minseok96@gmail.com>
2023-08-31 10:43:30 +0900

    feat : Not Found Page 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Issues from "./pages/Issues";
 import IssuesDetail from "./pages/IssuesDetail";
+import NotFound from "./pages/NotFound";
 const App = () => {
   return (
     <Router>
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Issues />} />
           <Route path="/:id" element={<IssuesDetail />}></Route>
+          <Route path="/*" element={<NotFound />}></Route>
         </Routes>
       </Layout>
     </Router>
